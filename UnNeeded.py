22ebb21349935f1c60bb797764c4cc15c6663441
Jake McCann <jakemccann2000@gmail.com>
2020-04-19 02:08:29 -0400

    Delete UnNeeded.py

--- UnNeeded.py
+++
@@ -1,5 +0,0 @@
-#Maui elevation map shape file, was needed for generating grid of elevation
-#fp = "maucntrs100.shp/maucntrs100.shp"
-#data = gpd.read_file(fp)
-#geoSet = data['geometry']
-#geoTree = STRtree(geoSet)
